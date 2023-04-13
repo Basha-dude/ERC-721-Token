fd4b2943c0263b375a0898423e686212d8a69ee8
Basha-dude <rebelbasha99@gmail.com>
2023-04-13 06:21:43 +0530

    Approve and getApproved completed

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -11,12 +11,19 @@ contract ERC721 {
     mapping (address => uint) private  _balance;
     mapping (uint => address) public  _owners;
     mapping (address => mapping (address => bool)) internal  operatorsApproval;
+    mapping (uint256 => address) internal  tokenIdApproval;
+
 
     event ApprovalForAll(
         address indexed _owner,
         address indexed _operator,
         bool _approved
     );
+     event Approval(
+        address indexed _owner,
+        address indexed _approved,
+       uint256 indexed _tokenId
+       );
 
     constructor (string memory _name, string memory _symbol) {
         name = _name;
@@ -42,4 +49,14 @@ contract ERC721 {
     function isApprovedForAll(address _owner, address _operator) public view returns (bool) {
         return  operatorsApproval[_owner][_operator];
     }
+    function approve(address _approved, uint256 _tokenId) public {
+           address owner = ownerOf(_tokenId);
+           require(msg.sender == owner || isApprovedForAll(owner,msg.sender));
+           tokenIdApproval[_tokenId] = _approved;
+                emit Approval(owner,_approved,_tokenId);
+    }
+    function getApproved(uint256 _tokenId) external view returns (address) {
+        require(_owners[_tokenId] != address(0));
+        return tokenIdApproval[_tokenId];
+    }     
 }
